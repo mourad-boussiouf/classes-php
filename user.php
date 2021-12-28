cc04e47686eff81d5a3e0fe506f381be696f47f3
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2021-12-28 17:03:22 +0100

    declaration des attributs de la classe user

---
+++ user.php
@@ -0,0 +1,28 @@
+<?php
+class User
+{
+  // Attributs
+private $id;
+public $login;
+public $email;
+public $firstname;
+public $lastname;
+}
+
+  // Methods
+  function __construct() {
+    $this->name = $name;
+  }
+  function get_name() {
+    return $this->name;
+  }
+  function set_color($color) {
+    $this->color = $color;
+  }
+  function get_color() {
+    return $this->color;
+  }
+}
+
+
+?>
